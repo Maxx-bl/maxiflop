37614b17ca292b08330cbbf64a175269f350e14f
maxx-bl <max13.bl@gmail.com>
2026-04-14 15:52:08 +0200

    changement musique menu + ajout mode fullscreen

--- maxiflop-src/scripts/SettingsManager.gd
+++ maxiflop-src/scripts/SettingsManager.gd
@@ -6,30 +6,38 @@ const SETTINGS_PATH = "user://settings.cfg"
 var in_game_volume: float = 100.0
 # Musique du menu (Slider 2)
 var menu_music_volume: float = 70.0
+# Plein écran
+var is_fullscreen: bool = false
 
 func _ready() -> void:
 	load_settings()
 
-func apply_volumes() -> void:
-	# Convert 0-100 linear value to dB
+func apply_settings() -> void:
+	# Volumes
 	var in_game_db := linear_to_db(in_game_volume / 100.0)
-	var menu_db := linear_to_db(menu_music_volume / 100.0)
+	var menu_music_db := linear_to_db(menu_music_volume / 100.0)
 	
-	# Appliquer aux bus respectifs
 	var idx_in_game = AudioServer.get_bus_index("InGameMusic")
 	if idx_in_game != -1:
 		AudioServer.set_bus_volume_db(idx_in_game, in_game_db)
 		
 	var idx_menu = AudioServer.get_bus_index("MenuMusic")
 	if idx_menu != -1:
-		AudioServer.set_bus_volume_db(idx_menu, menu_db)
+		AudioServer.set_bus_volume_db(idx_menu, menu_music_db)
+	
+	# Fullscreen
+	if is_fullscreen:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 
 func save_settings() -> void:
 	var config = ConfigFile.new()
 	config.set_value("audio", "in_game", in_game_volume)
 	config.set_value("audio", "menu_music", menu_music_volume)
+	config.set_value("display", "fullscreen", is_fullscreen)
 	config.save(SETTINGS_PATH)
-	apply_volumes()
+	apply_settings()
 
 func load_settings() -> void:
 	var config = ConfigFile.new()
@@ -38,8 +46,9 @@ func load_settings() -> void:
 	if err == OK:
 		in_game_volume = config.get_value("audio", "in_game", 100.0)
 		menu_music_volume = config.get_value("audio", "menu_music", 70.0)
+		is_fullscreen = config.get_value("display", "fullscreen", false)
 	
-	apply_volumes()
+	apply_settings()
 
 func linear_to_db(linear: float) -> float:
 	if linear <= 0.0001:
--- maxiflop-src/scripts/SettingsMenu.gd
+++ maxiflop-src/scripts/SettingsMenu.gd
@@ -2,15 +2,18 @@ extends Control
 
 @onready var in_game_slider: HSlider = %MasterSlider
 @onready var menu_slider: HSlider = %MusicSlider
+@onready var fullscreen_check: CheckBox = %FullscreenCheck
 
 func _ready() -> void:
-	# Initialise les sliders avec les nouvelles variables du manager
+	# Initialise les réglages depuis le manager
 	in_game_slider.value = SettingsManager.in_game_volume
 	menu_slider.value = SettingsManager.menu_music_volume
+	fullscreen_check.button_pressed = SettingsManager.is_fullscreen
 	
 	# Connecte les signaux
 	in_game_slider.value_changed.connect(_on_in_game_changed)
 	menu_slider.value_changed.connect(_on_menu_music_changed)
+	fullscreen_check.toggled.connect(_on_fullscreen_toggled)
 	
 	modulate.a = 0.0
 	var tween = create_tween()
@@ -18,11 +21,15 @@ func _ready() -> void:
 
 func _on_in_game_changed(value: float) -> void:
 	SettingsManager.in_game_volume = value
-	SettingsManager.apply_volumes()
+	SettingsManager.apply_settings()
 
 func _on_menu_music_changed(value: float) -> void:
 	SettingsManager.menu_music_volume = value
-	SettingsManager.apply_volumes()
+	SettingsManager.apply_settings()
+
+func _on_fullscreen_toggled(toggled_on: bool) -> void:
+	SettingsManager.is_fullscreen = toggled_on
+	SettingsManager.apply_settings()
 
 func _on_back_pressed() -> void:
 	# Sauvegarde les réglages avant de quitter
